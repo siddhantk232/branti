0020be6565fd8ecb4522c56128065e84666899ff
siddhantCodes <siddhantk232@gmail.com>
2022-04-29 01:19:59 +0530

    custom google fonts
    Rowdies
    Montserrat

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
   ],
   theme: {
     colors: {
-      accent: '#EE6C4D',
-      dark: '#212121',
-    }
-  }
-}
+      accent: "#EE6C4D",
+      dark: "#212121",
+    },
+    fontFamily: {
+      rowdies: ["Rowdies", "cursive"],
+      montsterrat: ["Montserrat", "sans-serif"],
+    },
+  },
+};
