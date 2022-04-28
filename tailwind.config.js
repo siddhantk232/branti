95b5b5b6fb2db8e05b194d4f1a5fccc27e4e5e8e
siddhantCodes <siddhantk232@gmail.com>
2022-04-29 01:08:32 +0530

    ui: application layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,11 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    colors: {
+      accent: '#EE6C4D',
+      dark: '#212121',
+    }
+  }
 }
