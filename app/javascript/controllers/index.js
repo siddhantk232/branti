6e5c88db4ee8cc91e150dca52207ec3d16f6f695
siddhantCodes <siddhantk232@gmail.com>
2022-04-30 21:33:20 +0530

    media player js implementation

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import PlayerController from "./player_controller.js"
+application.register("player", PlayerController)
