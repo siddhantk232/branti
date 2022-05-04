03019771992c79968c3b136ab058f8dc11e90570
siddhantCodes <siddhantk232@gmail.com>
2022-05-04 15:42:40 +0530

    designs for auth pages

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,6 +8,22 @@
 #
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
+
+
+class TurboFailureApp < Devise::FailureApp
+  def respond
+    if request_format == :turbo_stream
+      redirect
+    else
+      super
+    end
+  end
+
+  def skip_format?
+    %w(html turbo_stream */*).include? request_format.to_s
+  end
+end
+
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
@@ -279,6 +295,11 @@ Devise.setup do |config|
 
   OmniAuth.config.allowed_request_methods = [:get]
 
+  config.parent_controller = 'TurboController'
+
+  config.warden do |manager|
+    manager.failure_app = TurboFailureApp
+  end
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
